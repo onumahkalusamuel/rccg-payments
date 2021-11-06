85c479f501ca619d4a1aa87dc1513a305da4ff06
Onumah Kalu Samuel <onumahkalusamuel@gmail.com>
2021-11-06 21:24:36 +0100

    fixed favicon

--- index.php
+++ index.php
@@ -7,10 +7,6 @@
     <meta name="description" content="Contribution details for youth and choir joint programme.">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="manifest" href="site.webmanifest">
-    <link rel="apple-touch-icon" href="icon.png">
-    <!-- Place favicon.ico in the root directory -->
-
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/main.css">
     <meta name="theme-color" content="#fafafa">
